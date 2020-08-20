8c65236cdb4ee893ed9ade9bb3823d287e32da18
Yudi Riski <yudi.riski@mekari.com>
2020-08-20 09:42:26 +0700

    implement eslint linting with opinionated prettier format

--- index.js
+++ index.js
@@ -13,9 +13,11 @@ const db = new sqlite3.Database(':memory:');
 const buildSchemas = require('./src/schemas');
 
 db.serialize(() => {
-    buildSchemas(db);
+  buildSchemas(db);
 
-    const app = require('./src/app')(db);
+  const app = require('./src/app')(db);
 
-    app.listen(port, () => console.log(`App started and listening on port ${port}`));
+  app.listen(port, () =>
+    console.log(`App started and listening on port ${port}`)
+  );
 });
